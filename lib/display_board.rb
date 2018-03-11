a64f001b221b53f31a0c6db23a549b8a985d3fdd
Bruno Krauss <info@brunokrauss.com>
2018-03-11 20:03:18 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,5 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-O | O | O
------------
-O | O | O
------------
-O | O | O
 def display_board(board)
   puts "#{board[0]} | #{board[1]} | #{board[2]}"
   puts "-----------"
